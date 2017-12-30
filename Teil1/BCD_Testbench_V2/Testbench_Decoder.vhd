2910646de344d06b36c63216265db295d53217d9
Kumquad <werbungspam@outlook.com>
2017-12-30 12:51:58 +0100

    Bench1: Yay it's working (I think)

--- Teil1/BCD_Testbench_V2/Testbench_Decoder.vhd
+++ Teil1/BCD_Testbench_V2/Testbench_Decoder.vhd
@@ -38,6 +38,8 @@ ARCHITECTURE simulate OF Testbench_Decoder IS
 	signal StimSolution:		std_logic_vector (6 downto 0) := "0000000";
 	signal StimClock: 		std_logic :='0';
 	
+	
+
 --Component to Test
 component Decoder
 port (
@@ -50,7 +52,7 @@ end component;
 BEGIN
 
 --Generate clock
-StimClock <= not StimClock after 10ns;
+StimClock <= not StimClock after 10 ns;
 
 --Device to test 
 ConvertBcd_1: component Decoder
@@ -61,6 +63,9 @@ ConvertBcd_1: component Decoder
 		
 --Stimulate-process
 stimulate: PROCESS
+	
+	variable Increment:	integer range 0 to 9 :=0;
+	
 BEGIN
 
 --The DUT in this case is programmed asynchronous. It doesn't use a clock. No need to test for clocks here
@@ -69,107 +74,63 @@ while (StimClock/='0') loop
 	wait on StimClock;
 end loop;
 
-StimInput <= DataInput(0); --Sending a 0
-wait on StimClock;
-wait on StimClock;
-IF StimSolution = DataExpected(0) THEN
-	--its true. So its done
-	assert FALSE  report "Number 0: passed" severity Note;
-ELSE
-	--not true. Report error
-	assert FALSE report "Number 0: FAILED" severity Error;
-	--starting to test each bit for it's own
-	IF StimSolution(0) = DataExpected(0)(0) THEN
-		assert FALSE  report "  Position 0: passed" severity Note;
+for Increment in 0 to 9 loop
+	
+	--Sending the number
+	StimInput <= DataInput(Increment); 
+	
+	--wait for a rising edge
+	wait on StimClock;
+	wait on StimClock;
+	
+	--now test the outputs
+	IF StimSolution = DataExpected(Increment) THEN
+		--its true. So its done
+		assert FALSE  report "Number " & integer'image(Increment) & ": passed" severity Note;
 	ELSE
-		assert FALSE report "  Position 0: FAILED" severity Error;
+		--not true. Report error
+		assert FALSE report "Number " & integer'image(Increment) & " : FAILED" severity Error;
+		--starting to test each bit for it's own
+		IF StimSolution(0) = DataExpected(Increment)(0) THEN
+			assert FALSE  report "  Position 0: passed" severity Note;
+		ELSE
+			assert FALSE report "  Position 0: FAILED" severity Error;
+		END IF;
+		IF StimSolution(1) = DataExpected(Increment)(1) THEN
+			assert FALSE  report "  Position 1: passed" severity Note;
+		ELSE
+			assert FALSE report "  Position 1: FAILED" severity Error;
+		END IF;
+		IF StimSolution(2) = DataExpected(Increment)(2) THEN
+			assert FALSE  report "  Position 2: passed" severity Note;
+		ELSE
+			assert FALSE report "  Position 2: FAILED" severity Error;
+		END IF;
+		IF StimSolution(3) = DataExpected(Increment)(3) THEN
+			assert FALSE  report "  Position 3: passed" severity Note;
+		ELSE
+			assert FALSE report "  Position 3: FAILED" severity Error;
+		END IF;
+		IF StimSolution(4) = DataExpected(Increment)(4) THEN
+			assert FALSE  report "  Position 4: passed" severity Note;
+		ELSE
+			assert FALSE report "  Position 4: FAILED" severity Error;
+		END IF;
+		IF StimSolution(5) = DataExpected(Increment)(5) THEN
+			assert FALSE  report "  Position 5: passed" severity Note;
+		ELSE
+			assert FALSE report "  Position 5: FAILED" severity Error;
+		END IF;
+		IF StimSolution(6) = DataExpected(Increment)(6) THEN
+			assert FALSE  report "  Position 6: passed" severity Note;
+		ELSE
+			assert FALSE report "  Position 6: FAILED" severity Error;
+		END IF;
 	END IF;
-	IF StimSolution(1) = DataExpected(0)(1) THEN
-		assert FALSE  report "  Position 1: passed" severity Note;
-	ELSE
-		assert FALSE report "  Position 1: FAILED" severity Error;
-	END IF;
-	IF StimSolution(2) = DataExpected(0)(2) THEN
-		assert FALSE  report "  Position 2: passed" severity Note;
-	ELSE
-		assert FALSE report "  Position 2: FAILED" severity Error;
-	END IF;
-	IF StimSolution(3) = DataExpected(0)(3) THEN
-		assert FALSE  report "  Position 3: passed" severity Note;
-	ELSE
-		assert FALSE report "  Position 3: FAILED" severity Error;
-	END IF;
-	IF StimSolution(4) = DataExpected(0)(4) THEN
-		assert FALSE  report "  Position 4: passed" severity Note;
-	ELSE
-		assert FALSE report "  Position 4: FAILED" severity Error;
-	END IF;
-	IF StimSolution(5) = DataExpected(0)(5) THEN
-		assert FALSE  report "  Position 5: passed" severity Note;
-	ELSE
-		assert FALSE report "  Position 5: FAILED" severity Error;
-	END IF;
-	IF StimSolution(6) = DataExpected(0)(6) THEN
-		assert FALSE  report "  Position 6: passed" severity Note;
-	ELSE
-		assert FALSE report "  Position 6: FAILED" severity Error;
-	END IF;
-END IF;
-wait on StimClock;
-
---StimInput<="0001"; --Sending a 1
---wait on StimClock;
---assert StimSolution = "1001111" report "Number 1: passed" severity Note;	--The Seven-Seg is showing a 1. All good
---assert StimSolution /= "1001111" report "Number 1: FAILED" severity Error;	--The Seven-Seg is showing something else.
---wait on StimClock;
---
---StimInput<="0010"; --Sending a 2
---wait on StimClock;
---assert StimSolution = "0010010" report "Number 2: passed" severity Note;	--The Seven-Seg is showing a 2. All good
---assert StimSolution /= "0010010" report "Number 2: FAILED" severity Error;	--The Seven-Seg is showing something else.
---wait on StimClock;
---
---StimInput<="0011"; --Sending a 3
---wait on StimClock;
---assert StimSolution = "0000110" report "Number 3: passed" severity Note;	--The Seven-Seg is showing a 3. All good
---assert StimSolution /= "0000110" report "Number 3: FAILED" severity Error;	--The Seven-Seg is showing something else.
---wait on StimClock;
---
---StimInput<="0100"; --Sending a 4
---wait on StimClock;
---assert StimSolution /= "1001100" report "Number 4: passed" severity Note;	--The Seven-Seg is showing a 4. All good
---assert StimSolution = "1001100" report "Number 4: FAILED" severity Error;	--The Seven-Seg is showing something else.
---wait on StimClock;
---
---StimInput<="0101"; --Sending a 5
---wait on StimClock;
---assert StimSolution /= "0100100" report "Number 5: passed" severity Note;	--The Seven-Seg is showing a 5. All good
---assert StimSolution = "0100100" report "Number 5: FAILED" severity Error;	--The Seven-Seg is showing something else.
---wait on StimClock;
---
---StimInput<="0110"; --Sending a 6
---wait on StimClock;
---assert StimSolution /= "1100000" report "Number 6: passed" severity Note;	--The Seven-Seg is showing a 6. All good
---assert StimSolution = "1100000" report "Number 6: FAILED" severity Error;	--The Seven-Seg is showing something else.
---wait on StimClock;
---
---StimInput<="0111"; --Sending a 7
---wait on StimClock;
---assert StimSolution /= "0001111" report "Number 7: passed" severity Note;	--The Seven-Seg is showing a 7. All good
---assert StimSolution = "0001111" report "Number 7: FAILED" severity Error;	--The Seven-Seg is showing something else.
---wait on StimClock;
---
---StimInput<="1000"; --Sending a 8
---wait on StimClock;
---assert StimSolution /= "0000000" report "Number 8: passed" severity Note;	--The Seven-Seg is showing a 8. All good
---assert StimSolution = "0000000" report "Number 8: FAILED" severity Error;	--The Seven-Seg is showing something else.
---wait on StimClock;
---
---StimInput<="1001"; --Sending a 9
---wait on StimClock;
---assert StimSolution /= "0001100" report "Number 9: passed" severity Note;	--The Seven-Seg is showing a 9. All good
---assert StimSolution = "1110011" report "Number 9: FAILED" severity Error;	--The Seven-Seg is showing something else.
---wait on StimClock;
+	
+	wait on StimClock;
+	
+end loop;
 
 --Finished
 assert FALSE report "DONE!" severity NOTE;
