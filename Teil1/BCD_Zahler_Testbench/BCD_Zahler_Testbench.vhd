0bc6955684291fde30893091aaec7dadfef2fffa
fendt67 <fendt67@users.noreply.github.com>
2018-01-08 15:32:18 +0100

    BCD_Zahler _Testbench working

--- Teil1/BCD_Zahler_Testbench/BCD_Zahler_Testbench.vhd
+++ Teil1/BCD_Zahler_Testbench/BCD_Zahler_Testbench.vhd
@@ -5,128 +5,142 @@
 -- Matrikelnummer : 179156 --
 ------------------------------------------------------
 
--- Library Declaration --
+-- Library Declaration -------
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
-use ieee.numeric_std.all;
+USE ieee.numeric_std.all;
 
 
-------------------------------------------------------
---	   ENTITY	                           			 --
-------------------------------------------------------
+------------------------------
+-- 			ENTITY          --
+------------------------------
 
-ENTITY BCD_Zahler_Testbench IS
-END BCD_Zahler_Testbench;
+ENTITY BCD_zahler_Testbench IS
 
-------------------------------------------------------
---        ARCHITECTURE	                  			 --
-------------------------------------------------------
-ARCHITECTURE simu OF BCD_Zahler_Testbench IS
+END BCD_zahler_Testbench;
+
+
+-----------------------------
+--				ARCHITECTURE	--
+-----------------------------
+
+ARCHITECTURE TestBench of BCD_zahler_Testbench IS
+
+--Constant Signal
+CONSTANT LoopLimit : INTEGER := 120;
 
 --internal Signals /stimulate signals
-	signal StimClock: 		std_logic;
-	signal StimReset:			std_logic;
-	signal StimCM: 			std_logic;
-	signal Stimb0: 			std_logic;
-	signal Stimb1: 			std_logic;
-	signal Stimb2: 			std_logic;
-	signal Stimb3: 			std_logic;
+
+	SIGNAL StimReset : std_logic :='0';
+	SIGNAL StimClk : std_logic :='0';
+	SIGNAL StimCM : std_logic := '0';
+	SIGNAL Stimb : std_logic_vector (3 downto 0) := "0000";
 
 --Component to Test
-component BCD_zahler
-port (
-	reset,clk,CM	:IN	std_logic;
-	b0,b1,b2,b3		:OUT	std_logic
-	);
-end component;
+COMPONENT BCD_zahler
+------------------
+
+		PORT(		
+				Reset, Clk, CM		 :	IN std_logic;
+				b : OUT std_logic_vector (3 downto 0));
+
+END COMPONENT;
+
+----------------------------------------		
 
 BEGIN
 
---Generate clock
-StimClock <= not StimClock after 20 ns;
 
---Device to test 
-ConvertBcd_1: component Decoder
-		port map(
-				Input => StimInput,											
-				Output => StimSolution
-		);
+
+-- Clock Generator 50Mhz
+
+		StimClk <= not StimClk after 1 ps; 
 		
---Stimulate-process
-stimulate: PROCESS
-	
-	variable Increment:	integer range 0 to 9 :=0;
-	
-BEGIN
+		
+-- Call Component
 
---The DUT in this case is programmed asynchronous. It doesn't use a clock. No need to test for clocks here in theory.
---But the clock is used to slow things down, to have someting beautiful in the Wave-Graph during simulation
-wait on StimClock;
-while (StimClock/='0') loop
-	wait on StimClock;
-end loop;
 
---Starting main foor loop
-for Increment in 0 to 9 loop
+	Zahler: component BCD_Zahler
+			PORT MAP(
+							Reset => StimReset,
+							Clk	=> StimClk,
+							CM => StimCM,
+							b => Stimb);
 	
-	--Sending the number
-	StimInput <= DataInput(Increment); 
-	
-	--wait for a rising edge
-	wait on StimClock;
-	wait on StimClock;
-	
-	--now test the outputs
-	IF StimSolution = DataExpected(Increment) THEN
-		--its true. So its done
-		assert FALSE  report " Number " & integer'image(Increment) & ": passed" severity Note;
-	ELSE
-		--not true. Report error
-		assert FALSE report "Number " & integer'image(Increment) & " : FAILED" severity Error;
-		--starting to test each bit for it's own
-		IF StimSolution(0) = DataExpected(Increment)(0) THEN
-			assert FALSE  report "   Position 0: passed" severity Note;
-		ELSE
-			assert FALSE report "  Position 0: FAILED" severity Error;
-		END IF;
-		IF StimSolution(1) = DataExpected(Increment)(1) THEN
-			assert FALSE  report "   Position 1: passed" severity Note;
-		ELSE
-			assert FALSE report "  Position 1: FAILED" severity Error;
-		END IF;
-		IF StimSolution(2) = DataExpected(Increment)(2) THEN
-			assert FALSE  report "   Position 2: passed" severity Note;
-		ELSE
-			assert FALSE report "  Position 2: FAILED" severity Error;
-		END IF;
-		IF StimSolution(3) = DataExpected(Increment)(3) THEN
-			assert FALSE  report "   Position 3: passed" severity Note;
-		ELSE
-			assert FALSE report "  Position 3: FAILED" severity Error;
-		END IF;
-		IF StimSolution(4) = DataExpected(Increment)(4) THEN
-			assert FALSE  report "   Position 4: passed" severity Note;
-		ELSE
-			assert FALSE report "  Position 4: FAILED" severity Error;
-		END IF;
-		IF StimSolution(5) = DataExpected(Increment)(5) THEN
-			assert FALSE  report "   Position 5: passed" severity Note;
-		ELSE
-			assert FALSE report "  Position 5: FAILED" severity Error;
-		END IF;
-		IF StimSolution(6) = DataExpected(Increment)(6) THEN
-			assert FALSE  report "   Position 6: passed" severity Note;
-		ELSE
-			assert FALSE report "  Position 6: FAILED" severity Error;
-		END IF;
-	END IF;
-	
-end loop;
 
---Finished
-assert FALSE report "DONE!" severity NOTE;
-wait;
-	
-end process;
-	
-END simu;
+
+PROCESS 
+
+	VARIABLE LoopCounter : INTEGER := 0;
+
+BEGIN		
+
+		
+		--1) testing when CM = 1 if counter count 0 1 3 5 7 9 0 ...
+		--2) testing when CM = 0 if counter count 0 2 4 6 8 0 ...
+		
+		StimReset <= '1'; --doing reset
+		assert FALSE report "Reset 1" severity Note;
+		wait on StimClk; --waiting for the next rising edge
+		wait on StimClk; 
+		StimReset <= '0';
+		assert FALSE report "Reset 0" severity Note;
+		
+		
+		--1)
+		
+		--Reset Loop Counter
+		LoopCounter:=0;
+		--CM = 1
+		StimCM <= '1';		
+		
+		
+		L1 : loop 
+			exit L1 when (Stimb ="1001"); 
+			exit L1 when (LoopCounter > Looplimit); 
+			wait on StimClk;
+			wait on StimClk;
+			LoopCounter := LoopCounter + 1;
+			--assert FALSE report "While loop" & integer'image(LoopCounter) severity Note;
+		end loop;
+--		
+		
+		
+		IF (LoopCounter > Looplimit) THEN
+			assert FALSE report "Counting 0 1 3 5 7 9 has failled" severity Note;	
+		ELSE
+			assert FALSE report "Counting 0 1 3  5 7 9 is working" severity Note;
+		END IF;
+		
+		wait on StimClk;
+		wait on StimClk;
+		
+		--2)
+		
+		--Reset Countloop
+		LoopCounter := 0;
+		--CM = 0
+		StimCM <= '0';
+		
+		L2 : loop
+			exit L2 when (Stimb="1000");
+			exit L2 when (LoopCounter < LoopLimit);
+			wait on StimClk;
+			wait on StimClk;
+			LoopCounter := LoopCounter + 1;
+		end loop;
+		
+		IF (LoopCounter > Looplimit) THEN
+			assert FALSE report "Counting 0 2 4 6 8 has failled" severity Note;	
+		ELSE
+			assert FALSE report "Counting 0 2 4 6 8 is working" severity Note;
+		END IF;
+		
+		
+		
+		assert FALSE report "All tests passed" severity Note;	
+		wait;
+		
+END PROCESS;
+
+END TestBench;
