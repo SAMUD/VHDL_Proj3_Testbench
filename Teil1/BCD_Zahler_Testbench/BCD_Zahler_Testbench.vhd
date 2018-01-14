b40e0050b98a365aa5b8c6e0b8d58c0563c40178
fendt67 <fendt67@users.noreply.github.com>
2018-01-14 19:36:34 +0100

    added comments in StateMachine_testbench and BCD_zahler_Testbench

--- Teil1/BCD_Zahler_Testbench/BCD_Zahler_Testbench.vhd
+++ Teil1/BCD_Zahler_Testbench/BCD_Zahler_Testbench.vhd
@@ -41,13 +41,13 @@ CONSTANT LoopLimit : INTEGER := 15;
 	SIGNAL Valid_count_All : std_logic := '0';
 
 	TYPE DataExpected_ArrayOdd IS ARRAY (6 downto 0) 	of std_logic_vector (3 downto 0);
-	SIGNAL DataExpected_Odd : DataExpected_ArrayOdd := ("0000","1001","0111","0101","0011","0001","0000");
+	SIGNAL DataExpected_Odd : DataExpected_ArrayOdd := ("0000","1001","0111","0101","0011","0001","0000");	--Expecting value when odd counting
 		
 	TYPE DataExpected_ArrayEven IS ARRAY (5 downto 0) 	of std_logic_vector (3 downto 0);
-	SIGNAL DataExpected_Even : DataExpected_ArrayEven := ("0000","1000","0110","0100","0010","0000");
+	SIGNAL DataExpected_Even : DataExpected_ArrayEven := ("0000","1000","0110","0100","0010","0000");		--Expecting value when Even counting
 	
 	TYPE DataExpected_ArrayAll IS ARRAY (10 downto 0) 	of std_logic_vector (3 downto 0);
-	SIGNAL DataExpected_All : DataExpected_ArrayAll := ("0000","1001","1000","0111","0110","0101","0100","0011","0010","0001","0000");
+	SIGNAL DataExpected_All : DataExpected_ArrayAll := ("0000","1001","1000","0111","0110","0101","0100","0011","0010","0001","0000");	--Expecting value when Odd and Even counting
 		
 --Component to Test
 COMPONENT BCD_zahler
@@ -104,7 +104,7 @@ BEGIN
 		assert FALSE report "Reset 0" severity Note;
 		
 		
-		--1)
+--1)
 		
 		--Reset Loop Counter
 		LoopCounter:=0;
@@ -112,16 +112,17 @@ BEGIN
 		StimCM <= '1';		
 		
 		
-		L1 : FOR IncrementOdd IN 0 TO 6 loop
+		L1 : FOR IncrementOdd IN 0 TO 6 loop		--Loop to test all expecting value when odd counting
 			
 			L11 : loop
-				exit L11 when (Stimb = DataExpected_Odd(IncrementOdd)); 
-				exit L11 when (LoopCounter > Looplimit);
+				exit L11 when (Stimb = DataExpected_Odd(IncrementOdd)); --wait to the expected value
+				exit L11 when (LoopCounter > Looplimit);			--to prevent infinit loop
 				wait on StimClk;
 				wait on StimClk;
 				LoopCounter := LoopCounter + 1;
 			end loop;
 			
+			--output message depending on result of test
 			IF (LoopCounter > Looplimit) THEN
 				assert FALSE report "Expected: " & integer'image(to_integer(unsigned(DataExpected_Odd(IncrementOdd)))) & " but became: " & integer'image(to_integer(unsigned((Stimb))))severity Note;
 				Valid_count_Odd <='1';
@@ -134,9 +135,9 @@ BEGIN
 			LoopCounter := 0;
 			
 		end loop;
---		
+	
 		
-		
+		--Output message depending on the successfull of odd counting
 		IF Valid_count_Odd='1' THEN
 			assert FALSE report "Counting 0 1 3 5 7 9 has failled" severity Note;	
 		ELSE
@@ -146,23 +147,24 @@ BEGIN
 		wait on StimClk;
 		wait on StimClk;
 		
-		--2)
+--2)
 		
 		--Reset Countloop
 		LoopCounter := 0;
 		--CM = 0
 		StimCM <= '0';
 		
-		L2 : FOR IncrementEven IN 0 TO 5 loop
+		L2 : FOR IncrementEven IN 0 TO 5 loop		--Loop to test all expecting value when Even counting
 			
 			L21 : loop
-				exit L21 when (Stimb = DataExpected_Even(IncrementEven)); 
-				exit L21 when (LoopCounter > Looplimit);
+				exit L21 when (Stimb = DataExpected_Even(IncrementEven)); 	--wait to the expected value
+				exit L21 when (LoopCounter > Looplimit);			--to prevent infinit loop
 				wait on StimClk;
 				wait on StimClk;
 				LoopCounter := LoopCounter + 1;
 			end loop;
 			
+			--output message depending on result of test
 			IF (LoopCounter > Looplimit) THEN
 				assert FALSE report "Expected: " & integer'image(to_integer(unsigned(DataExpected_Even(IncrementEven)))) & " but became: " & integer'image(to_integer(unsigned((Stimb))))severity Note;
 				Valid_count_Even <='1';
@@ -175,9 +177,9 @@ BEGIN
 			LoopCounter := 0;
 			
 		end loop;
---		
+	
 		
-		
+		--Output message depending on the successfull of Even counting
 		IF Valid_count_Even='1' THEN
 			assert FALSE report "Counting 0 2 4 6 8 0 has failled" severity Note;	
 		ELSE
@@ -188,23 +190,24 @@ BEGIN
 		wait on StimClk;
 		
 		
-		--3)
+--3)
 		
 		--Reset Countloop
 		LoopCounter := 0;
 		--CM = 0
 		StimCM <= '0';
 		
-		L3 : FOR IncrementAll IN 0 TO 10 loop
+		L3 : FOR IncrementAll IN 0 TO 10 loop	--Loop to test all expecting value when alternaly odd and Even counting
 			
 			L31 : loop
-				exit L31 when (Stimb = DataExpected_All(IncrementAll)); 
-				exit L31 when (LoopCounter > Looplimit);
+				exit L31 when (Stimb = DataExpected_All(IncrementAll)); --wait to the expected value
+				exit L31 when (LoopCounter > Looplimit);			--to prevent infinit loop
 				wait on StimClk;
 				wait on StimClk;
 				LoopCounter := LoopCounter + 1;
 			end loop;
 			
+			--output message depending on result of test
 			IF (LoopCounter > Looplimit) THEN
 				assert FALSE report "Expected: " & integer'image(to_integer(unsigned(DataExpected_All(IncrementAll)))) & " but became: " & integer'image(to_integer(unsigned((Stimb))))severity Note;
 				Valid_count_All <='1';
@@ -223,9 +226,9 @@ BEGIN
 			END IF;
 			
 		end loop;
---		
+	
 		
-		
+		--Output message depending on the successfull of alternaly Even and odd counting
 		IF Valid_count_All='1' THEN
 			assert FALSE report "Counting 0 1 2 3 4 5 6 7 8 9 0 has failled" severity Note;	
 		ELSE
