89be411d47030b43b9ae6fb7bb4d6a75d9561c7c
Kumquad <werbungspam@outlook.com>
2018-01-14 19:58:45 +0100

    Testbench_all
    *added Counter

--- Teil2/Testbenches/Testbench_all.vhd
+++ Teil2/Testbenches/Testbench_all.vhd
@@ -40,8 +40,9 @@ SIGNAL Sim_Buzzer_Enable 				: std_logic :='0';
 SIGNAL Sim_Buzzer_BuzzerOut 			: std_logic :='0';
 
 -------------------------------------------------
--- Simulation Signals for testing ConvertIntToBcd
+--Simulation Signals for testing ConvertIntToBcd
 CONSTANT LoopLimit : INTEGER := 100;
+CONSTANT LoopLimitCountingDown : INTEGER := 1000000;
 --Signals which contain the Numbers to test and the expected outputs
 TYPE DataInput_ArrayMin IS ARRAY (10 downto 0) 	of INTEGER;
 SIGNAL DataInputMin : DataInput_ArrayMin := (0, 5400, 4800, 4200, 3600, 3000, 2400, 1800, 1200, 600, 0);
@@ -67,6 +68,21 @@ SIGNAL Stim_IntBcd_SevenSeg2	: std_logic_vector (6 downto 0) := "0000000";
 SIGNAL Stim_IntBcd_SevenSeg3	: std_logic_vector (6 downto 0) := "0000000";	
 SIGNAL Stim_IntBcd_SevenSeg4	: std_logic_vector (6 downto 0) := "0000000";
 
+-------------------------------------------------
+--Simulation Signals for testing Counter
+SIGNAL StimClk_Deci_i : std_logic :='0';
+SIGNAL StimCountBlockControl_i : std_logic_vector (5 downto 0) := "000000";
+SIGNAL StimBtnMinF_i : std_logic :='0';
+SIGNAL StimBtnSecF_i	: std_logic :='0';
+SIGNAL StimBtnMin_i	: std_logic :='0';
+SIGNAL StimBtnSec_i	: std_logic :='0';
+SIGNAL StimCountBlockTelemet_o : std_logic :='0';
+SIGNAL StimCountValue_o	: INTEGER range 0 to 6000;
+--Signals which contain the Numbers to test and the expected outputs
+TYPE DataExpected_Counter_ArrayMin IS ARRAY (10 downto 0) 	of INTEGER;
+SIGNAL DataExpected_Counter_Min : DataExpected_Counter_ArrayMin := (0, 5400, 4800, 4200, 3600, 3000, 2400, 1800, 1200, 600, 0);														
+TYPE DataExpected_ArraySec IS ARRAY (60 downto 0) 	of INTEGER;
+SIGNAL DataExpectedSec : DataExpected_ArraySec := (0, 590, 580, 570, 560, 550, 540, 530, 520, 510, 500, 490, 480, 470, 460, 450, 440, 430, 420, 410, 400, 390, 380, 370, 360, 350, 340, 330, 320, 310, 300, 290, 280, 270, 260, 250, 240, 230, 220, 210, 200, 190, 180, 170, 160, 150, 140, 130, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0);
 
 --Component to Test
 component ClockDivider
@@ -100,11 +116,37 @@ COMPONENT ConvertIntBcd
 			
 END COMPONENT;
 
+COMPONENT Counter
+	  PORT(
+			clk_i		 				:	IN		 std_logic;
+			clk_deci_i					:	IN		 std_logic;
+			--Control the Counter-Block
+			CountBlockControl_i 		:	IN	std_logic_vector(5 downto 0);	
+																				--MSB| Bit 5-4-3-2-1-0 | LSB
+																				--Bit 5 : Reset
+																				--Bit 4 : Counting has started / enable
+																				--Bit 3 : Ready for Incrementing the Value
+																				--Bit 2 : Load last Saved Value
+																				--Bit 1 : Save current Counter Value
+																				--Bit 0 : Enable Buzzer
+			CountBlockTelemet_o 		:	OUT	std_logic;				--Bit 0 : Counter is at 0
+			--Current Count Value
+			CountValue_o				:	OUT integer range 0 to 6000;
+			--User buttons
+			BtnMinF_i					:	IN		 std_logic;
+			BtnSecF_i					:	IN		 std_logic;
+			BtnMin_i					:	IN		 std_logic;
+			BtnSec_i					:	IN		 std_logic
+			);
+END COMPONENT;
+
 ---------------------------------------------
 BEGIN
 
 --Generate clock of 50 MHz
 Sim_clk <= not Sim_clk after 1 ps; --50Mhz normally duty-Cycle of 10ns
+-- Clock Dezi Generator 
+StimClk_deci_i <= not StimClk_deci_i after 100 ps; 	
 
 -- Component Clock Divider
 ClockDivider_1: component ClockDivider PORT MAP(
@@ -130,6 +172,24 @@ ConvertIntBcd_1: COMPONENT ConvertIntBcd
 				SevenSeg2 => Stim_IntBcd_SevenSeg2,
 				SevenSeg3 => Stim_IntBcd_SevenSeg3,
 				SevenSeg4 => Stim_IntBcd_SevenSeg4 
+				);	
+
+--Component Counter
+Counter_1: COMPONENT Counter
+		PORT MAP(
+				clk_i => Sim_clk,
+				clk_deci_i => StimClk_deci_i,
+				
+				--Control the Counter-Block
+				CountBlockControl_i => StimCountBlockControl_i,
+				CountBlockTelemet_o => StimCountBlockTelemet_o,
+				CountValue_o => StimCountValue_o,
+				
+				--User buttons
+				BtnMinF_i => StimBtnMinF_i,
+				BtnSecF_i => StimBtnSecF_i,
+				BtnMin_i => StimBtnMin_i,
+				BtnSec_i => StimBtnSec_i 
 				);	
 		
 --Stimulate-process
@@ -356,7 +416,7 @@ ClockDivider_Test: PROCESS
 			wait on Sim_clk;
 				
 			IF Stim_IntBcd_SevenSeg1 = DataExpectedMin(temp) THEN
-				assert FALSE  report "ConvertIntBcd: Min Number " & integer'image(temp) & ": passed" severity Note;
+				--assert FALSE  report "ConvertIntBcd: Min Number " & integer'image(temp) & ": passed" severity Note;
 			ELSE
 				assert FALSE report "ConvertIntBcd: Min Number " & integer'image(temp) & " : FAILED" severity Error;
 				ErrorCounter := ErrorCounter+1;
@@ -377,7 +437,7 @@ ClockDivider_Test: PROCESS
 			wait on Sim_clk;
 				
 			IF Stim_IntBcd_SevenSeg2 = DataExpectedSecLeft(temp) THEN
-				assert FALSE  report "ConvertIntBcd: Sec Left Number " & integer'image(temp) & ": passed" severity Note;
+				--assert FALSE  report "ConvertIntBcd: Sec Left Number " & integer'image(temp) & ": passed" severity Note;
 			ELSE
 				assert FALSE report "ConvertIntBcd: Sec Left Number " & integer'image(temp) & " : FAILED" severity Error;
 				ErrorCounter := ErrorCounter+1;
@@ -398,7 +458,7 @@ ClockDivider_Test: PROCESS
 			wait on Sim_clk;
 				
 			IF Stim_IntBcd_SevenSeg3 = DataExpectedSecRight(temp) THEN
-				assert FALSE  report "ConvertIntBcd: Sec Right Number " & integer'image(temp) & ": passed" severity Note;
+				--assert FALSE  report "ConvertIntBcd: Sec Right Number " & integer'image(temp) & ": passed" severity Note;
 			
 			ELSE
 				assert FALSE report "ConvertIntBcd: Sec Right Number " & integer'image(temp) & " : FAILED" severity Error;
@@ -420,7 +480,7 @@ ClockDivider_Test: PROCESS
 			wait on Sim_clk;
 		
 			IF Stim_IntBcd_SevenSeg4 = DataExpectedDezisec(temp) THEN
-				assert FALSE  report "ConvertIntBcd: Deziseconde Number " & integer'image(temp) & ": passed" severity Note;
+				--assert FALSE  report "ConvertIntBcd: Deziseconde Number " & integer'image(temp) & ": passed" severity Note;
 			
 			ELSE
 				assert FALSE report "ConvertIntBcd: Deziseconde Number " & integer'image(temp) & " : FAILED" severity Error;
@@ -436,12 +496,194 @@ ClockDivider_Test: PROCESS
 		end if ;
 
 -------------------------------------------------
---Testing ConvertIntToBcd
+--Testing Counter
 -------------------------------------------------
+		--1) Testing the incrementation of the Min to the max value
+		--2) Testing the incrementation of the Sec to the max value
+		--3) Testing the incrementation of the Min and Sec and Test Clear
+		--4) Testing the dekrementation of the value to zero
+		
+		StimCountBlockControl_i <= "100000"; --doing reset
+		wait on Sim_clk; --waiting for the next rising edge
+		wait on Sim_clk; 
+
+		--1)
+
+		StimCountBlockControl_i <= "001000";  --Incrementation Mode
+		--assert FALSE report "Counter: Mode Ready for Incrementing the Value" severity Note;
+		assert FALSE report "Counter: Begin Test Incrementing Min Value" severity Note;
+
+		Loop40: FOR temp IN 0 TO 10 LOOP
+				StimBtnMinF_i <= '1';  --Press Min Button
+				wait on Sim_clk; 	 --waiting for the next rising edge
+				wait on Sim_clk;				
+				
+				IF StimCountValue_o = DataExpected_Counter_Min(temp) THEN
+					--assert FALSE  report "Counter: Min " & integer'image(temp) & ": passed" severity Note;
+				ELSE
+					assert FALSE report "Counter: Min " & integer'image(temp) & " : FAILED" severity Error;
+					ErrorCounter := ErrorCounter+1;					
+				END IF;
+				
+				StimBtnMinF_i <= '0';  --Release Min Button
+				
+				wait on Sim_clk; 	 --waiting for the next rising edge
+				wait on Sim_clk;
+
+		END LOOP Loop40;
+		--assert FALSE report "Counter: Finishing Test Incrementing Min Value" severity Note;
+
+		StimCountBlockControl_i <= "100000"; --doing reset
+		--assert FALSE report "Counter: Mode Reset" severity Note;
+		wait on Sim_clk; --waiting for the next rising edge
+		wait on Sim_clk; 
+
+
+		--2)
+
+		StimCountBlockControl_i <= "001000";  --Incrementation Mode
+		--assert FALSE report "Counter: Mode Ready for Incrementing the Value" severity Note;
+		assert FALSE report "Counter: Begin Test Incrementing Sec Value" severity Note;
+
+		Loop41: FOR temp IN 0 TO 60 LOOP
+
+				StimBtnSecF_i <= '1';  --Press Sec Button
+				wait on Sim_clk; 	 --waiting for the next rising edge
+				wait on Sim_clk;				
+				
+				IF StimCountValue_o = DataExpectedSec(temp) THEN
+					--assert FALSE  report "Counter: Sec " & integer'image(temp) & ": passed" severity Note;
+				ELSE
+					assert FALSE report "Counter: Sec " & integer'image(temp) & " : FAILED" severity Error;
+					ErrorCounter := ErrorCounter+1;	
+				END IF;
+				
+				StimBtnSecF_i <= '0';  --Release Sec Button
+				wait on Sim_clk; 	 --waiting for the next rising edge
+				wait on Sim_clk;
+
+		END LOOP Loop41;
+		--assert FALSE report "Counter: Finishing Test Incrementing Sec Value" severity Note;
+
+		StimCountBlockControl_i <= "100000"; --doing reset
+		--assert FALSE report "Counter: Mode Reset" severity Note;
+		wait on Sim_clk; --waiting for the next rising edge
+		wait on Sim_clk; 
+
+		--3)
+
+		StimCountBlockControl_i <= "001000";  --Incrementation Mode
+		--assert FALSE report "Counter: Mode Ready for Incrementing the Value" severity Note;
+		--assert FALSE report "Counter: Begin Incrementing Sec Value" severity Note; 
+
+		Loop42: FOR temp IN 0 TO 20 LOOP
+
+				StimBtnSecF_i <= '1';  --Press Sec Button
+				wait on Sim_clk; 	 --waiting for the next rising edge
+				wait on Sim_clk;				
+	
+				StimBtnSecF_i <= '0';  --Release Sec Button
+				wait on Sim_clk; 	 --waiting for the next rising edge
+				wait on Sim_clk;
+
+		END LOOP Loop42;
+
+		--assert FALSE report "Counter: Finishing Incrementing Sec Value" severity Note;
+		--assert FALSE report "Counter: Begin Incrementing Min Value" severity Note; 
+
+		Loop43: FOR temp IN 0 TO 5 LOOP
+
+				StimBtnMinF_i <= '1';  --Press Min Button
+				wait on Sim_clk; 	 --waiting for the next rising edge
+				wait on Sim_clk;				
+				
+				StimBtnMinF_i <= '0';  --Release Min Button
+				wait on Sim_clk; 	 --waiting for the next rising edge
+				wait on Sim_clk;
+
+		END LOOP Loop43;
+
+		--assert FALSE report "Counter: Finishing Incrementing Sec Value" severity Note;
+		assert FALSE report "Counter: Test Clear Button" severity Note;
+
+		StimCountBlockControl_i <= "100000"; --doing reset
+
+		Loop44: LOOP
+				exit Loop44 when ((StimCountValue_o = 0) OR (StimCountBlockTelemet_o = '1'));
+				exit Loop44 when (LoopCounter > Looplimit);
+				wait on Sim_clk;
+				wait on Sim_clk;
+				LoopCounter := LoopCounter + 1;
+			
+		END LOOP Loop44;
+		--		
+		IF (LoopCounter > Looplimit) THEN
+			assert FALSE report "Counter: Clear Button has FAILED" severity Error;
+			ErrorCounter := ErrorCounter + 1;	
+		ELSE
+			--assert FALSE report "Counter: Clear Button : passed" severity Note;
+		END IF;
+		--assert FALSE report "Counter: Finishing Test Clear Mode" severity Note;
+
+		StimCountBlockControl_i <= "001000";  --Incrementation Mode
+		--assert FALSE report "Counter: Mode Ready for Incrementing the Value" severity Note;
+		--assert FALSE report "Counter: Begin Incrementing Sec Value" severity Note; 
+
+		Loop45: FOR temp IN 0 TO 2 LOOP
+
+				StimBtnSecF_i <= '1';  --Press Sec Button
+				wait on Sim_clk; 	 --waiting for the next rising edge
+				wait on Sim_clk;				
+				
+				StimBtnSecF_i <= '0';  --Release Sec Button
+				wait on Sim_clk; 	 --waiting for the next rising edge
+				wait on Sim_clk;
+
+		END LOOP Loop45;
+		--assert FALSE report "Counter: Finishing Incrementing Sec Value" severity Note;
+		--assert FALSE report "Counter: Begin Incrementing Min Value" severity Note; 
+
+		--Loop46: FOR temp IN 0 TO 1 LOOP
+
+				--StimBtnMinF_i <= '1';  --Press Min Button
+				--wait on Sim_clk; 	 --waiting for the next rising edge
+				--wait on Sim_clk;				
+				
+				--StimBtnMinF_i <= '0';  --Release Min Button
+				--wait on Sim_clk; 	 --waiting for the next rising edge
+				--wait on Sim_clk;
+
+		--END LOOP Loop46;
+
+		--assert FALSE report "Counter: Finishing Incrementing Min Value" severity Note;
+		assert FALSE report "Counter: Test Counting Down" severity Note;
+
+		StimCountBlockControl_i <= "010000"; --Mode Counting Down
+		LoopCounter := 0;
+
+		Loop47: LOOP
+				exit Loop47 when ((StimCountValue_o = 0) AND (StimCountBlockTelemet_o = '1'));
+				exit Loop47 when (LoopCounter > LoopLimitCountingDown);
+				wait on Sim_clk;
+				wait on Sim_clk;
+				LoopCounter := LoopCounter + 1;
+		END LOOP Loop47;
+
+		IF (LoopCounter > LoopLimitCountingDown) THEN
+			assert FALSE report "Counter: Counting Down has FAILED" severity Error;
+			ErrorCounter := ErrorCounter + 1;					
+		ELSE
+			assert FALSE report "Counter: Counting Down : passed" severity Note;
+		END IF;
+		--assert FALSE report "Counter: Finishing Test Counting Down" severity Note;
+
+		if ErrorCounter /= 0 then
+			assert FALSE report "Counter: Error inside" severity FAILURE;				
+		end if ;
 
 			
 		
-assert FALSE report "DONE!" severity NOTE;
+assert FALSE report "DONE with 0 Errors!" severity NOTE;
 		wait;
 			
 	END PROCESS;
