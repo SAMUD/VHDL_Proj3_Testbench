a38875351d3402075d5c8ccf2d195ce8af48da57
Kumquad <werbungspam@outlook.com>
2018-01-15 11:41:08 +0100

    less messages in TestbenchDecoder Teil1

--- Teil1/BCD_Decoder_Testbench_V2/Testbench_Decoder.vhd
+++ Teil1/BCD_Decoder_Testbench_V2/Testbench_Decoder.vhd
@@ -92,43 +92,43 @@ for Increment in 0 to 9 loop
 	--now test the outputs
 	IF StimSolution = DataExpected(Increment) THEN
 		--its true. So its done
-		assert FALSE  report " Number " & integer'image(Increment) & ": passed" severity Note;
+		--assert FALSE  report " Number " & integer'image(Increment) & ": passed" severity Note;
 	ELSE
 		--not true. Report error
 		assert FALSE report "Number " & integer'image(Increment) & " : FAILED" severity Error;
 		--starting to test each bit for it's own
 		IF StimSolution(0) = DataExpected(Increment)(0) THEN
-			assert FALSE  report "   Position 0: passed" severity Note;
+			--assert FALSE  report "   Position 0: passed" severity Note;
 		ELSE
 			assert FALSE report "  Position 0: FAILED" severity Error;
 		END IF;
 		IF StimSolution(1) = DataExpected(Increment)(1) THEN
-			assert FALSE  report "   Position 1: passed" severity Note;
+			--assert FALSE  report "   Position 1: passed" severity Note;
 		ELSE
 			assert FALSE report "  Position 1: FAILED" severity Error;
 		END IF;
 		IF StimSolution(2) = DataExpected(Increment)(2) THEN
-			assert FALSE  report "   Position 2: passed" severity Note;
+			--assert FALSE  report "   Position 2: passed" severity Note;
 		ELSE
 			assert FALSE report "  Position 2: FAILED" severity Error;
 		END IF;
 		IF StimSolution(3) = DataExpected(Increment)(3) THEN
-			assert FALSE  report "   Position 3: passed" severity Note;
+			--assert FALSE  report "   Position 3: passed" severity Note;
 		ELSE
 			assert FALSE report "  Position 3: FAILED" severity Error;
 		END IF;
 		IF StimSolution(4) = DataExpected(Increment)(4) THEN
-			assert FALSE  report "   Position 4: passed" severity Note;
+			--assert FALSE  report "   Position 4: passed" severity Note;
 		ELSE
 			assert FALSE report "  Position 4: FAILED" severity Error;
 		END IF;
 		IF StimSolution(5) = DataExpected(Increment)(5) THEN
-			assert FALSE  report "   Position 5: passed" severity Note;
+			--assert FALSE  report "   Position 5: passed" severity Note;
 		ELSE
 			assert FALSE report "  Position 5: FAILED" severity Error;
 		END IF;
 		IF StimSolution(6) = DataExpected(Increment)(6) THEN
-			assert FALSE  report "   Position 6: passed" severity Note;
+			--assert FALSE  report "   Position 6: passed" severity Note;
 		ELSE
 			assert FALSE report "  Position 6: FAILED" severity Error;
 		END IF;
